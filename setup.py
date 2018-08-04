8325bf7da8526f6be26c9b39008b9172f1a99964
Tyler Shake <tyler.shake@raytheon.com>
2018-08-04 15:25:59 -0400

    adjusted handling the config file for the package

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
     author="[organization]",
     description=("An example flask dashboard template"),
     license="MIT",
-    packages=['flask_dashboard'],
+    packages=['flask_dashboard', 'config'],
     long_description=read('README.md'),
     include_package_data=True,
     scripts=['flask_dashboard/flask_dashboard'],
